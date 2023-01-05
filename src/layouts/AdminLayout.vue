9124642f38585622ce1132a1708aba68728cfc0a
Leonssizm <levani.kereselidze11@gmail.com>
2023-01-05 22:20:43 +0400

    wip: admin panel sidebar & header

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AdminHeader from "../components/layouts/admin/Header.vue";
+import AdminSidebar from "../components/layouts/admin/Sidebar.vue";
+</script>
+
+<template>
+  <div class="flex-row">
+    <AdminHeader />
+    <AdminSidebar />
+  </div>
+</template>
