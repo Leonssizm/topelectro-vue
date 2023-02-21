82a086251f2dc404f663bd93bf50b1ed10093c49
Leonssizm <levani.kereselidze11@gmail.com>
2023-02-21 22:01:42 +0400

    wip:edit product

--- src/stores/useProductsStore.js
+++ src/stores/useProductsStore.js
@@ -11,31 +11,36 @@ export const useProductsStore = defineStore("products", {
     deleteProduct(id) {
       this.list = this.list.filter((product) => product.id != id);
     },
+    updateProducts(product) {
+      console.log(product);
+      const index = this.list.findIndex((item) => item.id == product.id);
+      this.list[index] = product;
+    },
   },
   getters: {
     getQuantity() {
       return this.list.length;
     },
-    getProductCategories() {
-      const categories = [];
-      const pivotData = [];
-      this.list.forEach((product) => {
-        product.categoryNames = [];
-        categories.push(product.categories);
-      });
-      categories.forEach((category) => {
-        category.forEach((item) => {
-          pivotData.push([item.pivot, item.name]);
-        });
-      });
-      pivotData.forEach((data) => {
-        this.list.forEach((product) => {
-          if (data[0].product_id == product.id) {
-            product.categoryNames.push(data[1]);
-          }
-        });
-      });
-      return this.list.categoryNames;
-    },
+    // getProductCategories() {
+    //   const categories = [];
+    //   const pivotData = [];
+    //   this.list.forEach((product) => {
+    //     product.categoryNames = [];
+    //     categories.push(product.categories);
+    //   });
+    //   categories.forEach((category) => {
+    //     category.forEach((item) => {
+    //       pivotData.push([item.pivot, item.name]);
+    //     });
+    //   });
+    //   pivotData.forEach((data) => {
+    //     this.list.forEach((product) => {
+    //       if (data[0].product_id == product.id) {
+    //         product.categoryNames.push(data[1]);
+    //       }
+    //     });
+    //   });
+    //   return this.list.categoryNames;
+    // },
   },
 });
