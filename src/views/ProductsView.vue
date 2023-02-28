1cea8f3d09e5d9f87618604ed4385c5369b3c288
Leonssizm <levani.kereselidze11@gmail.com>
2023-02-28 17:34:41 +0400

    refactor: productsView scripts change to setup script

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -7,14 +7,14 @@
         <div class="flex flex-col">
           <div class="overflow-x-auto">
             <div class="mt-3 border-indigo-500 py-3 pl-5 hover:border-l-4">
-              <p>Total of {{ this.store.getQuantity }} Products</p>
+              <p>Total of {{ store.getQuantity }} Products</p>
             </div>
             <div class="flex justify-between py-3 pl-2">
               <SearchInput />
               <div class="flex text-sm">
                 <ButtonPrimary
                   content="Add Product"
-                  @click="this.displayAddModal = true"
+                  @click="displayAddModal = true"
                 />
               </div>
             </div>
@@ -25,12 +25,12 @@
     </div>
   </div>
   <ProductsAddModal
-    @closeAddModal="this.displayAddModal = false"
-    v-if="this.displayAddModal"
+    @closeAddModal="displayAddModal = false"
+    v-if="displayAddModal"
   />
 </template>
 
-<script>
+<script setup>
 import ResourceHeader from "../components/shared/ResourceHeader.vue";
 import SearchInput from "../components/ui/inputs/SearchInput.vue";
 import ButtonPrimary from "@/components/ui/buttons/ButtonPrimary.vue";
@@ -38,49 +38,19 @@ import ProductsTable from "@/components/products/ProductsTable.vue";
 import ProductsAddModal from "@/components/products/ProductsModalAdd.vue";
 import axios from "@/plugins/axios/index.js";
 
+import { ref, onMounted } from "vue";
 import { useProductsStore } from "@/stores/useProductsStore";
-import { storeToRefs } from "pinia";
 
-export default {
-  components: {
-    SearchInput,
-    ResourceHeader,
-    ButtonPrimary,
-    ProductsTable,
-    ProductsAddModal,
-  },
-  data() {
-    return {
-      categories: [],
-      selectedCategory: "",
-      selectedCategoriesArr: [],
-      selectedCategoryId: [],
-      productId: "",
-      displayAddModal: false,
-    };
-  },
+const store = useProductsStore();
 
-  mounted() {
-    axios.get("products").then((response) => {
-      const products = response.data;
-      this.store.setProducts(products);
-    });
-  },
-  methods: {
-    getCategory() {
-      let selectedCategoryId = this.selectedCategory.split(",")[0];
-      this.selectedCategoryId.push(selectedCategoryId);
-    },
-  },
-  setup() {
-    const store = useProductsStore();
-    const { list } = storeToRefs(store);
-    return {
-      store,
-      list,
-    };
-  },
-};
+let displayAddModal = ref(false);
+
+onMounted(() => {
+  axios.get("products").then((response) => {
+    const products = response.data;
+    store.setProducts(products);
+  });
+});
 </script>
 
 <style>
