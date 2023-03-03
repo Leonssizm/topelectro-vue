d4bacb1d2b39ffc0f79d9d4b0e4954078ea4bd8b
Leonssizm <levani.kereselidze11@gmail.com>
2023-03-03 10:11:28 +0400

    feat: vuetifyed edit product form modal

--- src/components/products/ProductsTable.vue
+++ src/components/products/ProductsTable.vue
@@ -35,18 +35,12 @@
             </router-link>
           </button>
         </td>
-        <td
-          class="whitespace-nowrap px-6 py-4 text-center text-sm font-medium"
-          @click="displayProductValuesInEditModal(product, product.id)"
-        >
+        <td @click="displayProductValuesInEditModal(product, product.id)">
           <button type="button" class="text-green-500 hover:text-green-700">
             <IconEdit />
           </button>
         </td>
-        <td
-          class="whitespace-nowrap px-6 py-4 text-center text-sm font-medium"
-          @click="deleteProduct(product.id)"
-        >
+        <td @click="deleteProduct(product.id)">
           <button type="button" class="text-red-500 hover:text-red-700">
             <IconDelete />
           </button>
@@ -54,254 +48,99 @@
       </tr>
     </tbody>
   </v-table>
-
-  <!-- <div class="inline-block w-full p-1.5 align-middle">
-    <div class="overflow-hidden rounded-lg border">
-      <table class="table-fixed divide-y divide-gray-200">
-        <thead class="bg-gray-50">
-          <tr>
-            <th
-              scope="col"
-              class="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
-            >
-              ID
-            </th>
-            <th
-              scope="col"
-              class="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
-            >
-              SQ
-            </th>
-            <th
-              scope="col"
-              class="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
-            >
-              Name
-            </th>
-            <th
-              scope="col"
-              class="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
-            >
-              Price
-            </th>
-            <th
-              scope="col"
-              class="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
-            >
-              Wholesale_price
-            </th>
-            <th
-              scope="col"
-              class="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
-            >
-              Main image
-            </th>
-            <th
-              scope="col"
-              class="px-6 py-3 text-left text-xs font-bold uppercase text-gray-500"
-            >
-              Categories
-            </th>
-            <th
-              scope="col"
-              class="px-6 py-3 text-right text-xs font-bold uppercase text-gray-500"
-            >
-              Details
-            </th>
-            <th
-              scope="col"
-              class="px-6 py-3 text-right text-xs font-bold uppercase text-gray-500"
-            >
-              Edit
-            </th>
-
-            <th
-              scope="col"
-              class="px-6 py-3 text-right text-xs font-bold uppercase text-gray-500"
-            >
-              Delete
-            </th>
-          </tr>
-        </thead>
-        <tbody
-          v-for="product in productsStore.list"
-          :id="product.id"
-          :key="product.id"
-          class="divide-y divide-gray-200"
-        >
-          <tr>
-            <td
-              class="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-800"
-            >
-              {{ product.id }}
-            </td>
-            <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800">
-              {{ product.sq }}
-            </td>
-            <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800">
-              {{ product.name }}
-            </td>
-            <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800">
-              {{ product.price }}
-            </td>
-            <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800">
-              {{ product.wholesale_price }}
-            </td>
-            <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800">
-              {{ product.picture }}
-            </td>
-            <td class="whitespace-nowrap px-6 py-4 text-sm text-gray-800">
-              {{ product.categoryNames }}
-            </td>
-            <td
-              class="whitespace-nowrap px-6 py-4 text-center text-sm font-medium"
-            >
-              <button type="button" class="text-gray-500 hover:text-gray-700">
-                <router-link
-                  :to="{ name: 'details', params: { id: product.id } }"
-                >
-                  <IconDetails />
-                </router-link>
-              </button>
-            </td>
-            <td
-              class="whitespace-nowrap px-6 py-4 text-center text-sm font-medium"
-              @click="displayProductValuesInEditModal(product, product.id)"
-            >
-              <button type="button" class="text-green-500 hover:text-green-700">
-                <IconEdit />
-              </button>
-            </td>
-
-            <td
-              class="whitespace-nowrap px-6 py-4 text-center text-sm font-medium"
-              @click="deleteProduct(product.id)"
-            >
-              <button type="button" class="text-red-500 hover:text-red-700">
-                <IconDelete />
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div> -->
-
   <!-- Edit Product Modal -->
   <div class="visible" v-if="displayEditProductModal">
-    <div class="flex w-1/3 flex-col rounded border bg-green-100">
-      <div class="mt-5 flex justify-around">
-        <div class="mb-4 px-2">
-          <label class="mb-1 block text-sm">Product Name:</label>
-          <input
-            class="focus:shadow-outline w-full rounded border px-4 py-2 outline-none focus:border-green-300"
-            type="text"
-            placeholder="Name"
-            v-model="productName"
-          />
-        </div>
-        <div class="mb-4 px-2">
-          <label class="mb-1 block text-sm">SQ:</label>
-          <input
-            class="focus:shadow-outline w-full rounded border px-4 py-2 outline-none focus:border-green-300"
-            type="text"
-            placeholder="SQ"
-            v-model="productSQ"
-          />
-        </div>
-      </div>
-      <div class="flex justify-around">
-        <div class="mb-4 px-2">
-          <label class="mb-1 block text-sm">Price:</label>
-          <input
-            class="focus:shadow-outline w-full rounded border px-4 py-2 outline-none focus:border-green-300"
-            type="text"
-            placeholder="Price"
-            v-model="productPrice"
-          />
-        </div>
-        <div class="mb-4 px-2">
-          <label class="mb-1 block text-sm">Wholesale Price:</label>
-          <input
-            class="focus:shadow-outline w-full rounded border px-4 py-2 outline-none focus:border-green-300"
-            type="text"
-            placeholder="Wholesale Price"
-            v-model="productWholesalePrice"
-          />
-        </div>
-      </div>
-      <div class="flex justify-around">
-        <div class="px-2">
-          <div class="flex flex-col items-center">
-            <label class="mb-1 block text-sm">Image:</label>
-            <input
-              type="file"
-              class="text-grey-500 text-sm file:mr-5 file:rounded-full file:border-0 file:bg-blue-50 file:py-2 file:px-6 file:text-sm file:font-medium file:text-blue-700 hover:file:cursor-pointer hover:file:bg-amber-50 hover:file:text-amber-700"
-              accept="image"
-              @change="pictureDisplayAndStore"
-            />
-          </div>
-        </div>
-        <div class="px-2">
-          <div class="flex flex-col items-center">
-            <label class="mb-1 block text-sm">Color:</label>
-            <input
-              type="text"
-              class="focus:shadow-outline w-full rounded border px-4 py-2 outline-none focus:border-green-300"
-              placeholder="color"
-              v-model="productColor"
-            />
-          </div>
-        </div>
-      </div>
-      <div id="preview" class="mt-8 flex justify-center">
-        <img v-if="imageUrl" :src="imageUrl" />
-      </div>
-      <!-- Select category -->
-      <div>
-        <div class="mb-4 flex px-2">
-          <div class="mb-3 xl:w-96">
-            <label>Select Category</label>
-            <select
-              class="form-select m-0 block w-full appearance-none rounded border border-solid border-gray-300 bg-white bg-clip-padding bg-no-repeat px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
-              aria-label="Default select example"
-              id="categoriesSelect"
-              v-model="selectedCategoryId"
-              @change="getChosenCategories"
+    <div class="flex w-1/3 flex-col rounded border bg-gray-100">
+      <v-row justify="center">
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">Edit product</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Name"
+                    v-model="productName"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Product SQ"
+                    v-model="productSQ"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Product Color"
+                    v-model="productColor"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Price"
+                    v-model="productPrice"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Wholesale Price"
+                    v-model="productWholesalePrice"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-select
+                    :items="selectItems"
+                    item-title="state"
+                    item-value="id"
+                    return-object
+                    multiple
+                    v-model="selectedCategories"
+                  ></v-select>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="details"
+                    v-model="productDetails"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-file-input
+                    label="File input"
+                    variant="filled"
+                    prepend-icon="mdi-camera"
+                    @change="pictureDisplayAndStore"
+                    show-size
+                    counter
+                  ></v-file-input>
+                </v-col>
+                <div id="preview" class="flex justify-center">
+                  <v-img
+                    v-if="imageUrl"
+                    :src="imageUrl"
+                    :width="130"
+                    aspect-ratio="16/9"
+                    cover
+                  ></v-img>
+                </div>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions class="flex justify-between">
+            <v-btn color="blue-darken-1" variant="text" @click="editProduct">
+              Save Changes
+            </v-btn>
+            <v-btn
+              color="red"
+              variant="text"
+              @click="displayEditProductModal = false"
             >
-              <option hidden>Select Category</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <div>{{ selectedCategories }}</div>
-      <div class="mb-4 px-2">
-        <div class="flex flex-col">
-          <label class="mb-1 block text-sm">Details:</label>
-          <textarea
-            type="text"
-            class="focus:shadow-outline w-full rounded border px-4 py-2 outline-none focus:border-green-300"
-            placeholder="specify details"
-            v-model="productDetails"
-          ></textarea>
-        </div>
-      </div>
-      <div class="mb-2 flex justify-around">
-        <button
-          class="rounded bg-green-500 py-2 px-3 font-bold text-white hover:bg-green-700"
-          type="button"
-          @click="editProduct"
-        >
-          Save Changes
-        </button>
-        <button
-          type="button"
-          class="rounded bg-red-500 py-2 px-3 font-bold text-white hover:bg-red-700"
-          @click="displayEditProductModal = false"
-        >
-          Close
-        </button>
-      </div>
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-row>
     </div>
   </div>
 </template>
@@ -327,8 +166,8 @@ let image = ref("");
 let imageUrl = ref("");
 let productId = ref("");
 
-let selectedCategories = [];
-let selectedCategoryId = ref("");
+let selectItems = ref([]);
+let selectedCategories = ref([]);
 
 function deleteProduct(id) {
   axios.delete(`products/${id}`).then(() => {
@@ -347,9 +186,7 @@ function displayProductValuesInEditModal(product, id) {
   productId.value = id;
   axios.get("categories").then((response) => {
     response.data.forEach((category) => {
-      document.getElementById("categoriesSelect").innerHTML += `
-      <option value=${category.id} id=${category.id}>${category.name}</option>
-      `;
+      selectItems.value.push({ state: category.name, id: category.id });
     });
   });
 }
@@ -360,17 +197,10 @@ function pictureDisplayAndStore(e) {
   imageUrl.value = URL.createObjectURL(file);
 }
 
-function getChosenCategories() {
-  if (
-    selectedCategories.find((id) => id == selectedCategoryId.value) == undefined
-  ) {
-    selectedCategories.push(selectedCategoryId.value);
-  }
-}
-
 function editProduct() {
   // Sending request
   let formData = new FormData();
+
   formData.append("_method", "put");
   formData.append("name", productName.value);
   formData.append("sq", productSQ.value);
@@ -379,8 +209,8 @@ function editProduct() {
   formData.append("wholesale_price", productWholesalePrice.value);
   formData.append("details", productDetails.value);
   formData.append("picture", image.value);
-  for (let i = 0; i < selectedCategories.length; i++) {
-    formData.append(`category_ids[${i}]`, selectedCategories[i]);
+  for (let i = 0; i < selectedCategories.value.length; i++) {
+    formData.append(`category_ids[${i}]`, selectedCategories.value[i].id);
   }
   axios
     .post(`products/${productId.value}`, formData, {
