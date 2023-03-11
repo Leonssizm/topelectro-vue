cca2259d6bb5bb75ae84610de75ea53e504e813c
Leonssizm <levani.kereselidze11@gmail.com>
2023-03-11 12:22:08 +0400

    chore: add playground

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import ProductsView from "@/views/ProductsView.vue";
 import UsersView from "@/views/UsersView.vue";
 import CommentsView from "@/views/CommentsView.vue";
 import ProductDetails from "@/components/products/ProductDetails.vue";
+import PlaygroundView from "@/views/PlaygroundView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -38,6 +39,11 @@ const router = createRouter({
       path: "/comments",
       name: "comments",
       component: CommentsView,
+    },
+    {
+      path: "/playground",
+      name: "playground",
+      component: PlaygroundView,
     },
   ],
 });
