c97f86c38fa32baaa5f4ea277f519aaeb35b135d
Leonssizm <levani.kereselidze11@gmail.com>
2023-03-12 13:20:13 +0400

    feat: edit category modal validations

--- src/components/categories/CategoriesModalEdit.vue
+++ src/components/categories/CategoriesModalEdit.vue
@@ -1,31 +1,40 @@
 <template>
   <div class="visible">
     <div class="flex w-1/4 flex-col rounded border bg-gray-100">
-      <v-form>
+      <Form @submit="sendEditedCategoryInfo">
         <v-container>
-          <v-text-field
+          <Field
+            name="name"
+            as="v-text-field"
             type="text"
-            placeholder="Category"
             v-model="categoryName"
-            label="Category Name"
-          ></v-text-field>
-
-          <v-text-field
+            label="სახელი"
+            rules="required|min:3"
+            hide-details
+            placeholder="Category Name"
+          />
+          <ErrorMessage name="name" class="text-red-500" />
+          <Field
+            name="description"
+            as="v-text-field"
             type="text"
             placeholder="Description"
             v-model="categoryDescription"
-            label="Description"
-          ></v-text-field>
+            label="აღწერა"
+            rules="required|min:3"
+            hide-details
+          />
+          <ErrorMessage name="description" class="text-red-500" />
         </v-container>
         <v-card-actions class="flex justify-between pt-0">
-          <v-btn @click="sendEditedCategoryInfo" variant="flat" color="primary">
-            Save Changes</v-btn
-          >
+          <button>
+            <v-btn variant="flat" color="primary"> Save Changes</v-btn>
+          </button>
           <v-btn variant="flat" color="error" @click="$emit('closeEditModal')">
             Close
           </v-btn>
         </v-card-actions>
-      </v-form>
+      </Form>
     </div>
   </div>
 </template>
@@ -34,8 +43,11 @@
 import axios from "@/plugins/axios/index.js";
 
 import { ref, inject, reactive } from "vue";
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { setLocale } from "@vee-validate/i18n";
 
 const emit = defineEmits(["closeEditModal", "updateCategory"]);
+setLocale("ka");
 
 let name = inject("name").value;
 let description = inject("description").value;
